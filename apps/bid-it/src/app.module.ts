1cf8a137e794908598cccdf7420372d90c1bebaa
malcolm-kee <malcolm.keeweesiong@gmail.com>
2020-03-04 17:54:12 +0800

    initial setup

--- apps/bid-it/src/app.module.ts
+++ apps/bid-it/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DEALS_DB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Logger],
 })
 export class AppModule {}
