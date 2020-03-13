25b72715c980ed6f95234c3e2592126ddd70b491
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-03-14 01:16:12 +0800

    remove unused code

--- apps/bid-engine/src/app.type.ts
+++
@@ -1,8 +0,0 @@
-export const BID_QUEUE = 'BID_QUEUE';
-
-export interface PlaceBidData {
-  readonly bidId: string;
-  readonly dealId: string;
-  readonly dealerId: string;
-  readonly price: number;
-}
