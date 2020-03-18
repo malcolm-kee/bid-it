eef71fa99e31d52bf3279ed8355036701949d8b6
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-03-18 09:23:39 +0000

    add run simulation script

---
+++ run-simulation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+result="$(curl -s http://localhost:3000/deal)";
+
+echo $result;
+
+if [ "$result" == '[]' ]; then
+	echo "empty deal, we will create some deals for testing...";
+	yarn setup;
+else
+	echo "not empty, so we will not create more deal.";
+fi
+
+yarn simulate;
+
+export DB_HOST="$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' bid-it_db_1)";
+
+yarn report;
+
